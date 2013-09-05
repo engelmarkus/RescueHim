c1b70c064ec97316c50d6fcc9d6d629ef080841a
Markus Engel <markus.engel@tum.de>
2013-09-05 23:26:43 +0200

    Added some custom string literal for handling durations.

---
+++ src/ChronoLiterals.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <chrono>
+
+// These literals are about to be added to std::chrono_literals in c++14...
+
+/**
+ * @brief Literal to ease notation of durations taking milliseconds.
+ * @details Example Usage:
+ * @code
+ * std::chrono::duration half_a_second = 500_ms;
+ * @endcode
+ * @see std::chrono::duration
+ */
+constexpr std::chrono::milliseconds operator "" _ms(unsigned long long c) {
+	return std::chrono::milliseconds(c);
+}
+
