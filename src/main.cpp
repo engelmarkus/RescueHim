d48e874fdd8e442ab0bad40c429a4f14a00690d5
Markus Engel <markus.engel@tum.de>
2013-09-11 22:50:14 +0200

    Removed duplicate log line.

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ int main(int argc, char* argv[]) {
     try {
         SDL::instance().initialize(SDL_INIT_VIDEO);
     } catch (const SdlError& error) {
-        std::cerr << "Error while initializing SDL: " << error.what() << std::endl;
         AppLog<Severity::Error>::log("Error while initializing SDL: ", error.what());
         return 1;
     }
